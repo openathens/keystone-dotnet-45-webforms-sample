e131149c86b23c99cc66e4db9adcfc66d2d3538d
Julian Clementson <38796088+julianclem@users.noreply.github.com>
2019-12-03 22:14:23 +0000

    Add a page to show OIDC claims

---
+++ KeystoneDotNet45WebFormsSample/Claims.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Claims" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Claims.aspx.cs" Inherits="KeystoneDotNet45WebFormsSample.Claims" %>
+<%@ Import Namespace="System.Security.Claims" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>OpenID Connect Claims</h2>
+    <dl>
+        <asp:DataList runat="server" ID="dlClaims">
+            <ItemTemplate>
+                <dt><%# ((Claim) Container.DataItem).Type %></dt>
+                <dd><%# ((Claim) Container.DataItem).Value %></dd>
+            </ItemTemplate>
+        </asp:DataList>
+    </dl>
+</asp:Content>
---
+++ KeystoneDotNet45WebFormsSample/Claims.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KeystoneDotNet45WebFormsSample
+{
+    public partial class Claims : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.Current.GetOwinContext().Authentication.Challenge();
+            }
+
+            var claims = ClaimsPrincipal.Current.Claims;
+            dlClaims.DataSource = claims;
+            dlClaims.DataBind();
+        }
+    }
+}
